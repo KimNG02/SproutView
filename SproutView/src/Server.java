c9cdf8845badc781c5c42a1d20a65925b39c92df
Cadde0 <ocaddeo@kth.se>
2024-04-15 15:53:52 +0200

    test changes

--- SproutView/src/Server.java
+++ SproutView/src/Server.java
@@ -7,52 +7,55 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+//String OK = "HTTP/1.1 200 OK\r\nContent-Length: " + res.length() + "\r\n\r\n";
 
 public class Server {
-    
-    static String res = "<!DOCTYPE html>" + "<html lang=\"en\">" + "<html>" + "<head>" + "<title>Page Title</title>" + "</head>" + "<h1>Sprouuuuttt vieewwwww</h1>" + "</body>" + "</html>";
 
     static String BadRequest = "HTTP/1.1 400 Bad Request\r\n";
 
-    public Server(int portNumber) throws IOException{
-        
+    public Server(int portNumber) throws IOException {
+
         ServerSocket serverSocket = new ServerSocket(portNumber);
 
-        BufferedReader fileInput = new BufferedReader(new FileReader("SproutView/src/test.html")); 
-
-        String res = "";
-
-        while(true){
-            String responseAddition = fileInput.readLine();
-            if(responseAddition == null){
-                break;
-            }
-            res += responseAddition;
-        }
-
-        String OK = "HTTP/1.1 200 OK\r\nContent-Length: " + res.length() + "\r\n\r\n" ;
-
-        while(true){
+        while (true) {
             Socket socket = serverSocket.accept();
 
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            
+
             String line = input.readLine();
 
-            BufferedWriter output = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            byte[] outputBytes = null;
 
-            if(line.substring(0,14).equals("GET / HTTP/1.1")){
-                System.out.println("Line: " + line);
-                System.out.println("Connection");
-                
-                output.write(OK + res + "\r\n");
-                output.newLine();
-                output.flush();
+            if (line.substring(0, 3).equals("GET / ")) {
+                Path path = Paths.get("SproutView/resources/index.html");
+
+                String ok = "HTTP/1.1 200 OK\r\nContent-Length: " + "2000" + "\r\n\r\n";
+                byte[] okBytes = ok.getBytes();
+
+                byte[] fileBytes = Files.readAllBytes(path);
+
+                outputBytes = new byte[okBytes.length + fileBytes.length];
+
+                int n;
+                for(n = 0; n < okBytes.length; n++){
+                    outputBytes[n] = okBytes[n];
+                }
+
+                for(n = okBytes.length; n < okBytes.length + fileBytes.length; n++){
+                    outputBytes[n] = fileBytes[n - okBytes.length];
+                }
             }
-            else{
-                output.write(BadRequest);
-                output.newLine();
-                output.flush();
+
+            OutputStreamWriter output = new OutputStreamWriter(socket.getOutputStream());
+
+            if (outputBytes != null) {
+                for (int n = 0; n < outputBytes.length; n++) {
+                    output.write(outputBytes[n]);
+                }
             }
 
             socket.close();
