f5f63d6fd8cc61aed359b7eaaaa184a82c8a879f
UltraGhostie <38960155+UltraGhostie@users.noreply.github.com>
2024-04-16 16:07:35 +0200

    Test page

--- sprout-view/src/main/java/se/kth/ii1305/gulsparv/sproutview/APITest.java
+++ sprout-view/src/main/java/se/kth/ii1305/gulsparv/sproutview/APITest.java
@@ -1,40 +1,46 @@
 package se.kth.ii1305.gulsparv.sproutview;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
-import org.apache.tomcat.util.http.parser.MediaType;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
+import java.nio.file.Path;
 
 @RestController
 @RequestMapping("/test")
 public class APITest {
 
     @GetMapping
-    public ResponseEntity<String> Test()
+    public ResponseEntity<byte[]> Test()
     {
-        return new ResponseEntity<String>("Test", HttpStatusCode.valueOf(200));
+        byte[] fileBytes = {};
+        String userDirectory = new File("").getAbsolutePath();
+        System.out.println(userDirectory);
+        Path path = Paths.get("sprout-view/src/main/resources/static/test.html"); 
+        try {
+            fileBytes = Files.readAllBytes(path);
+        } catch (Exception e) {
+            System.out.println("ERROR");
+        }
+        return new ResponseEntity<byte[]>(fileBytes, HttpStatusCode.valueOf(200));
     }
 
-    @GetMapping(value = "/strawberryTransparent.png", produces = "image/png")
-    public ResponseEntity<Resource> image() throws IOException {
-        final ByteArrayResource inputStream = new ByteArrayResource(Files.readAllBytes(Paths.get(
-                "/strawberryTransparent.png"
-        )));
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .contentLength(inputStream.contentLength())
-                .body(inputStream);
+    // IMAGE MAPPING
+    // @GetMapping(value = "/strawberryTransparent.png", produces = "image/png")
+    // public ResponseEntity<Resource> image() throws IOException {
+    //     final ByteArrayResource inputStream = new ByteArrayResource(Files.readAllBytes(Paths.get(
+    //             "/strawberryTransparent.png"
+    //     )));
+    //     return ResponseEntity
+    //             .status(HttpStatus.OK)
+    //             .contentLength(inputStream.contentLength())
+    //             .body(inputStream);
 
-    }
+    // }
 }
 
