539e7c093891fd21a9ccbb411d605fe833439682
Cadde0 <ocaddeo@kth.se>
2024-04-16 11:11:25 +0200

    Fixed Image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-
+import React from 'react';
 import './App.css';
+import strawberryImage from './Strawberry transparent.png';
 
 function App() {
   return (
     <div>
-      Hello World
+      <header className="logo">
+        Sprout View 🪴    
+      </header>
+      <main>
+        <div className="searchBarContainer">
+          <input
+            className="searchBar"
+            type="text"
+            placeholder="Search plants..."
+          />
+        </div>
+        <div className="imageContainer">
+          <img 
+            className="plantImage" 
+            src={strawberryImage} 
+            alt="Strawberry"
+            onClick={() => window.location.href = 'https://www.facebook.com/'}
+          />
+        </div>
+      </main>
     </div>
   );
 }
