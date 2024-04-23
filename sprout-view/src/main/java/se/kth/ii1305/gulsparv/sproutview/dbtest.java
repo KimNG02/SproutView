157d4edfb0b5721fe243995ce780ccc39b267773
UltraGhostie <38960155+UltraGhostie@users.noreply.github.com>
2024-04-23 09:11:35 +0200

    dbupdate

--- sprout-view/src/main/java/se/kth/ii1305/gulsparv/sproutview/dbtest.java
+++ sprout-view/src/main/java/se/kth/ii1305/gulsparv/sproutview/dbtest.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.*;
@@ -12,15 +13,15 @@ import java.sql.*;
 @RequestMapping("api/db")
 public class dbtest {
 
-    String password = "password";
+    String password = "7631";
 
     @GetMapping
-    public ResponseEntity<String> test() {
+    public ResponseEntity<String> test(@RequestParam String plant) {
         String string = "test";
         Connection db;
         try {
             db = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/sprouttest", "postgres", password);
-            ResultSet result = db.prepareStatement("SELECT * FROM plant;").executeQuery();
+            ResultSet result = db.prepareStatement(String.format("SELECT * FROM plant WHERE name = '%s';", plant)).executeQuery();
             result.next();
             string = result.getString(2);
         } catch (Exception e) {
